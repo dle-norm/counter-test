384fc7b211a0994ceb6f97134432d3f6cfede80b
dlenormand <dlenormand@inbox.fr>
2022-05-20 10:56:06 +0200

    fix:fix CI

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AppComponent } from './app.component';
 import { DownComponent } from './down/down.component';
 import { ResetComponent } from './reset/reset.component';
 import { UpComponent } from './up/up.component';
